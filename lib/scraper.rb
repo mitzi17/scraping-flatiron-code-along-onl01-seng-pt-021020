1a0c8c40c4ed4a55cbe734c22a0b5c575d821aee
Mitzi Estrada <m_estrada17@live.com>
2020-03-19 07:06:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
+    binding.pry
     doc.css(".post").each do |post|
       course = Course.new
       course.title = post.css("h2").text
@@ -16,7 +16,7 @@ class Scraper
       course.description = post.css("p").text
       
     
-    binding.pry  
+      
     end
   end
 end
