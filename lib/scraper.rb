cc4a7c95d063c3d749825a4b98a4566984afad6c
Mitzi Estrada <m_estrada17@live.com>
2020-03-19 07:09:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,10 +14,11 @@ class Scraper
       #course.title = post.css("h2").text
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
-      
-    
-      
     end
+    
+  def get_courses
+    self.get_page.css(".post")
+  end
   
 end
     
